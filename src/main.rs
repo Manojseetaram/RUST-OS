41f1e1bbd487aababce0dc131de6307b64e42866
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-03 14:03:00 +0530

    impl Writer for vga mode

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-// src/main.rs
 
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
 
+#![no_std] 
+#![no_main] 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -11,18 +11,11 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Rust is not a cult!";
+
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    vga_buffer::print_something();
+   
     loop {}
 }
