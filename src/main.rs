c6bed684c885061f6da15b0c1179272122084eb8
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-03 20:16:08 +0530

    add testble traits

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,19 @@
 mod vga_buffer;
 mod serial;
 use core::panic::PanicInfo;
+pub trait Testable {
+    fn run(&self);
+}
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
 pub enum QemuExitCode {
     Success = 0x10,
     Failed = 0x11,
@@ -38,24 +51,31 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Rust is not a Cult {}" , "!");
 //    use core::fmt::Write;
 //    vga_buffer::WRITER.lock().write_str("Rust is Not a Cult").unwrap();
 //    write!(vga_buffer::WRITER.lock() , ", some numbers : {} {}" , 42 , 1.337).unwrap();
-println!("Hello , Word {}", "!");
+
 #[cfg(test)]
 test_main();
+
+#[allow(clippy::empty_loop)]
 
     loop {}
 }
 
 
 #[cfg(test)]
-fn test_runner(tests : &[&dyn Fn()]){
-   serial_println!("Running {} tests" , tests.len() );
-   for test in tests{
-    test();
-   }
-   exit_qemu(QemuExitCode::Success);
+pub fn test_runner(tests: &[&dyn Testable]) { 
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run(); 
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
 #[test_case]
 fn trivial_assertaion(){
