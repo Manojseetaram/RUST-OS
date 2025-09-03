2a7d9d43d6d8fe76b6116dd4dd97e8720f01eb66
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-03 18:28:14 +0530

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 
-
 #![no_std] 
 #![no_main] 
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 mod vga_buffer;
 use core::panic::PanicInfo;
+pub enum QeumExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+pub fn exit_qemu(exit_code : QeumExitCode){
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -20,6 +34,23 @@ pub extern "C" fn _start() -> ! {
 //    vga_buffer::WRITER.lock().write_str("Rust is Not a Cult").unwrap();
 //    write!(vga_buffer::WRITER.lock() , ", some numbers : {} {}" , 42 , 1.337).unwrap();
 println!("Hello , Word {}", "!");
-panic!("Some panic Messege");
+#[cfg(test)]
+test_main();
+
     loop {}
+}
+
+
+#[cfg(test)]
+fn test_runner(tests : &[&dyn Fn()]){
+   println!("Running {} tests" , tests.len() );
+   for test in tests{
+    test();
+   }
+}
+#[test_case]
+fn trivial_assertaion(){
+    print!("taival assertion... ");
+    assert_eq!(1 , 1);
+    println!("[ok]")
 }
