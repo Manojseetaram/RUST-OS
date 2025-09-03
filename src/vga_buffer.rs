aeff5ed33e3758fea83b0a11e7c5ec8768e44fae
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-03 14:19:49 +0530

    use volatile for screen writes

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,7 @@
+use volatile::Volatile;
 #[allow(dead_code)]
+
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
@@ -42,7 +45,7 @@ const BUFFER_WIDTH: usize = 25;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 pub struct Writer {
     column_position: usize,
@@ -62,14 +65,16 @@ impl Writer {
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
     }
+
+
 fn write_string(&mut self , s : &str){
    for byte in s.bytes(){
     match  byte {
