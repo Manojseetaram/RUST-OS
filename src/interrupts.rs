7a92608841bff9d1d1e57e7b5944a1ec1eccc27c
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-04 09:55:32 +0530

    interrupts testing

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -23,3 +23,7 @@ extern "x86-interrupt" fn breakpoint_handler(
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
+#[test_case]
+fn test_breakpoint_exception(){
+    x86_64::instructions::interrupts::int3();
+}
