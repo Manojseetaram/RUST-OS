cbef8761ed514e34b17f863ad058635baadef8ed
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-04 19:06:33 +0530

    modify the kernel function useing BootInfoFrameAllocater

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use blog_os::println;
+use blog_os::{memory, println};
 use bootloader::{BootInfo, entry_point};
-use x86_64::structures::paging::Page;
+
 
 entry_point!(kernel_main);
 pub trait Testable {
@@ -30,33 +30,35 @@ fn panic(info: &PanicInfo) -> ! {
 }
 #[unsafe(no_mangle)]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    
-
-    use blog_os::memory;
-    use x86_64::{structures::paging::Page, VirtAddr };
+    use blog_os::memory::BootInfoFrameAllocator;
+    use x86_64::VirtAddr;
       
     println!("Rust is  Not a cult{}", "!");
-   blog_os::init();
+    blog_os::init();
 
 
 
-  let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-  let mut mapper = unsafe { memory::init(phys_mem_offset) };
-  let mut frame_allocater = memory::EmptyFrameAllocator;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut _mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut _frame_allocator = unsafe {  BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
    
-//mapo unused page 
-let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-memory::create_example_mapping(page,&mut  mapper,&mut frame_allocater);
 
-//writing the string New! to the screen throught the new mapping
 
-let page_ptr : *mut u64 = page.start_address().as_mut_ptr();
-unsafe {page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
-    #[cfg(test)]
+     #[cfg(test)]
     test_main();
-  println!("It did not crash!");
+    println!("It did not crash!");
     
-   blog_os::hlt_loop()
+    blog_os::hlt_loop()
+//mapo unused page 
+// let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000b8000));
+// memory::create_example_mapping(page,&mut  mapper,&mut frame_allocater);
+
+// //writing the string New! to the screen throught the new mapping
+
+// let page_ptr : *mut u64 = page.start_address().as_mut_ptr();
+// unsafe {page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+  
        
 }
 
--- src/memory.rs
+++ src/memory.rs
@@ -6,45 +6,14 @@ use x86_64::{
     PhysAddr
 };
 use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
-pub struct BootInfoFrameAllocator {
-    memory_map: &'static MemoryMap,
-    next: usize,
-}
+
 pub struct EmptyFrameAllocator;
 unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
         None
     }
 }
-impl BootInfoFrameAllocator {
-   /// Returns an iterator over the usable frames specified in the memory map.
-    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
-        // get usable regions from memory map
-        let regions = self.memory_map.iter();
-        let usable_regions = regions
-            .filter(|r| r.region_type == MemoryRegionType::Usable);
-        // map each region to its address range
-        let addr_ranges = usable_regions
-            .map(|r| r.range.start_addr()..r.range.end_addr());
-        // transform to an iterator of frame start addresses
-        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
-        // create `PhysFrame` types from the start addresses
-        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
-    }
-    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
-        BootInfoFrameAllocator {
-            memory_map,
-            next: 0,
-        }
-    }
-}
-unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
-    fn allocate_frame(&mut self) -> Option<PhysFrame> {
-        let frame = self.usable_frames().nth(self.next);
-        self.next += 1;
-        frame
-    }
-}
+
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     unsafe {
         let level_4_table = active_level_4_table(physical_memory_offset);
@@ -124,4 +93,41 @@ pub fn create_example_mapping(
         mapper.map_to(page, frame, flags, frame_allocator)
     };
     map_to_result.expect("map_to failed").flush();
+}
+
+//-----------------------------------------------------------------------------------------------
+//Bootinfo allocater
+//---------------------------------------------------------------------------------------------
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+impl BootInfoFrameAllocator {
+   /// Returns an iterator over the usable frames specified in the memory map.
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        // get usable regions from memory map
+        let regions = self.memory_map.iter();
+        let usable_regions = regions
+            .filter(|r| r.region_type == MemoryRegionType::Usable);
+        // map each region to its address range
+        let addr_ranges = usable_regions
+            .map(|r| r.range.start_addr()..r.range.end_addr());
+        // transform to an iterator of frame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // create `PhysFrame` types from the start addresses
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+}
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
 }
