a5b9c5cd785c7dcfaa493ffb0fc803d6321f595a
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-04 21:58:22 +0530

    alloc from region functions

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,5 +1,6 @@
-use alloc::collections::linked_list::LinkedList;
 
+use super::align_up;
+use core::mem;
 struct ListNode {
     size: usize,
     next: Option<&'static mut ListNode>,
@@ -34,7 +35,62 @@ impl LinkedListAllocator {
     }
 
     /// Adds the given memory region to the front of the list.
-    unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
-        todo!();
+   unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        // ensure that the freed region is capable of holding ListNode
+        assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
+        assert!(size >= mem::size_of::<ListNode>());
+
+        // create a new list node and append it at the start of the list
+        let mut node = ListNode::new(size);
+        node.next = self.head.next.take();
+        let node_ptr = addr as *mut ListNode;
+        unsafe {
+            node_ptr.write(node);
+            self.head.next = Some(&mut *node_ptr)
+        }
+    }
+
+fn find_region(&mut self, size: usize, align: usize)
+        -> Option<(&'static mut ListNode, usize)>
+    {
+        // reference to current list node, updated for each iteration
+        let mut current = &mut self.head;
+        // look for a large enough memory region in linked list
+        while let Some(ref mut region) = current.next {
+            if let Ok(alloc_start) = Self::alloc_from_region(&region, size, align) {
+                // region suitable for allocation -> remove node from list
+                let next = region.next.take();
+                let ret = Some((current.next.take().unwrap(), alloc_start));
+                current.next = next;
+                return ret;
+            } else {
+                // region not suitable -> continue with next region
+                current = current.next.as_mut().unwrap();
+            }
+        }
+
+        // no suitable region found
+        None
+    }
+      fn alloc_from_region(region: &ListNode, size: usize, align: usize)
+        -> Result<usize, ()>
+    {
+        let alloc_start = align_up(region.start_addr(), align);
+        let alloc_end = alloc_start.checked_add(size).ok_or(())?;
+
+        if alloc_end > region.end_addr() {
+            // region too small
+            return Err(());
+        }
+
+        let excess_size = region.end_addr() - alloc_end;
+        if excess_size > 0 && excess_size < mem::size_of::<ListNode>() {
+            // rest of region too small to hold a ListNode (required because the
+            // allocation splits the region in a used and a free part)
+            return Err(());
+        }
+
+        // region suitable for allocation
+        Ok(alloc_start)
     }
 }
