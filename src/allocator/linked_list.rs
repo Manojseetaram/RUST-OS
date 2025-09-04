4dbf5797d2a91c03ad05f42e21f0af607487b6b5
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-04 21:44:53 +0530

    implment the method for listnode

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,6 +1,11 @@
+use alloc::collections::linked_list::LinkedList;
+
 struct ListNode {
     size: usize,
     next: Option<&'static mut ListNode>,
+}
+pub struct LinkedListAllocator{
+    head : ListNode,
 }
 impl ListNode {
     const fn new(size: usize) -> Self {
@@ -13,5 +18,23 @@ impl ListNode {
 
     fn end_addr(&self) -> usize {
         self.start_addr() + self.size
+    }
+}
+impl LinkedListAllocator {
+     pub const fn new() -> Self {
+        Self {
+            head: ListNode::new(0),
+        }
+    }
+
+     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        unsafe {
+            self.add_free_region(heap_start, heap_size);
+        }
+    }
+
+    /// Adds the given memory region to the front of the list.
+    unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        todo!();
     }
 }
