e6e1981c175920ec62adef0e4f88349357f13104
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-04 17:46:17 +0530

    showing leve4 and lvel3 page table

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 use core::panic::PanicInfo;
 use blog_os::println;
 use bootloader::{BootInfo, entry_point};
+
 entry_point!(kernel_main);
 pub trait Testable {
     fn run(&self);
@@ -30,7 +31,7 @@ fn panic(info: &PanicInfo) -> ! {
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
      use blog_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
-
+  use x86_64::structures::paging::PageTable;
     println!("Rust is  Not a cult{}", "!");
    blog_os::init();
 
@@ -46,8 +47,21 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    println!("L4 Entry {}: {:?}", i, entry);
+
+    // get the physical address from the entry and convert it
+    let phys = entry.frame().unwrap().start_address();
+    let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    let ptr = VirtAddr::new(virt).as_mut_ptr();
+    let l3_table: &PageTable = unsafe { &*ptr };
+
+    // print non-empty entries of the level 3 table
+    for (i, entry) in l3_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("  L3 Entry {}: {:?}", i, entry);
         }
+    }
+}
     }
 
     use x86_64::registers::control::Cr3;
