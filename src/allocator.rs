2032623294f011331f46deff74bbd39eb1a897c7
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-09-04 21:16:38 +0530

    implement GlobalAlloc for our bump allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,6 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-
+pub mod bump;
 // #[cfg(feature = "use_dummy")]
 #[global_allocator]
 
@@ -17,7 +17,21 @@ use linked_list_allocator::LockedHeap;
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; 
 pub struct Dummy;
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
 
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
     frame_allocator: &mut impl FrameAllocator<Size4KiB>,
